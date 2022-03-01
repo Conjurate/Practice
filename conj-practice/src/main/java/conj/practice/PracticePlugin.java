b4f82a0c72e5e5aed4d66998587ff98a0e537d30
Conjurate <amtrixspark@gmail.com>
2022-03-01 12:39:27 -0700

    first commit

---
+++ conj-practice/src/main/java/conj/practice/PracticePlugin.java
@@ -0,0 +1,15 @@
+package conj.practice;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class PracticePlugin extends JavaPlugin {
+
+	public void onEnable() {
+		
+	}
+	
+	public void onDisable() {
+		
+	}
+	
+}
