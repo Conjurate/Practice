868f1cba346591e37622a6a553683d3965c49298
Conjurate <amtrixspark@gmail.com>
2022-03-27 23:48:20 -0700

    added debug info

--- conj-practice/src/main/java/conj/practice/PracticePlugin.java
+++ conj-practice/src/main/java/conj/practice/PracticePlugin.java
@@ -1,15 +1,26 @@
 package conj.practice;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class PracticePlugin extends JavaPlugin {
 
+	public static boolean debug = true;
+	
 	public void onEnable() {
 		
 	}
 	
 	public void onDisable() {
 		
+	}
+	
+	public static void debug(Object...msg) {
+		if (debug) {
+			for(Object o : msg) {
+				Bukkit.getLogger().info(o.toString());
+			}
+		}
 	}
 	
 }
