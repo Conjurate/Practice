668a1bbd5aabc8082217a04b48db1eb85a2a5a22
Conjurate <amtrixspark@gmail.com>
2022-03-28 00:49:39 -0700

    kill mob play villager sound

--- conj-practice/src/main/java/conj/practice/PracticeListener.java
+++ conj-practice/src/main/java/conj/practice/PracticeListener.java
@@ -1,9 +1,15 @@
 package conj.practice;
 
 import org.bukkit.Material;
+import org.bukkit.Sound;
 import org.bukkit.block.Block;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 
 public class PracticeListener implements Listener {
@@ -15,6 +21,19 @@ public class PracticeListener implements Listener {
 			b.setType(Material.GLOWSTONE);
 		}
 		
+	}
+	
+	@EventHandler
+	public void killMobSound(EntityDamageByEntityEvent event) {
+		Entity defender = event.getEntity();
+		Entity attacker = event.getDamager();
+		if (defender.getType().isAlive() && attacker.getType() == EntityType.PLAYER) {
+			LivingEntity living = (LivingEntity) defender;
+			Player player = (Player) attacker;
+			if (living.getHealth()-event.getFinalDamage() <= 0) {
+				player.getWorld().playSound(player, Sound.ENTITY_VILLAGER_YES, 10, 1);
+			}
+		}
 	}
 	
 }
