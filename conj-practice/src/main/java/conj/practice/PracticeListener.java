7280691b40f4df188465867338e89dc1ce2abd4d
Nahom <nahomdesta442@gmail.com>
2022-03-29 13:33:14 -0400

    Added grass block interaction sound

--- conj-practice/src/main/java/conj/practice/PracticeListener.java
+++ conj-practice/src/main/java/conj/practice/PracticeListener.java
@@ -9,20 +9,21 @@ import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 
 public class PracticeListener implements Listener {
 
 	@EventHandler
-	public void changeBlock(PlayerInteractEvent e){
+	public void changeBlock(PlayerInteractEvent e) {
 		Block b = e.getClickedBlock();
 		if (b.getType() == Material.GOLD_BLOCK) {
 			b.setType(Material.GLOWSTONE);
 		}
-		
+
 	}
-	
+
 	@EventHandler
 	public void killMobSound(EntityDamageByEntityEvent event) {
 		Entity defender = event.getEntity();
@@ -30,10 +31,18 @@ public class PracticeListener implements Listener {
 		if (defender.getType().isAlive() && attacker.getType() == EntityType.PLAYER) {
 			LivingEntity living = (LivingEntity) defender;
 			Player player = (Player) attacker;
-			if (living.getHealth()-event.getFinalDamage() <= 0) {
+			if (living.getHealth() - event.getFinalDamage() <= 0) {
 				player.getWorld().playSound(player, Sound.ENTITY_VILLAGER_YES, 10, 1);
 			}
 		}
 	}
-	
+	@EventHandler
+	public void interactBlockSound(PlayerInteractEvent e) {
+		Block b = e.getClickedBlock();
+		Player player = e.getPlayer();
+		if (b.getType() == Material.GRASS_BLOCK && e.getAction() == Action.RIGHT_CLICK_BLOCK) {
+			player.getWorld().playSound(player, Sound.ENTITY_VILLAGER_YES, 10, 1);
+		}
+	}
+
 }
